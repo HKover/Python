265a5ba473ad0fa9751ad9ff57d4f2db46cdb4ce
AbramSunshine <pedalimagazine@gmail.com>
2025-01-23 15:20:02 -0500

    1commit

---
+++ lesson1/lesson2/vetvlenia.py
@@ -0,0 +1,17 @@
+# string = "Hel23lo world"
+
+# # if "Hello" not in string:
+# #     print("Hello is have")
+# # elif "world" in string:
+# #     print("world is string")
+
+# # else:
+# #     print("Hello is not have")
+
+
+# if "Hello2" in string:
+#     print("Hello is a string ")
+# elif string == "Hello world":
+#     print("string = Hello world")
+# else:
+#     print("World is not string")
