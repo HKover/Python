381f85210d58f98296a1023909e738459be4d2f0
HackHunter <pedalimagazine@gmail.com>
2025-01-29 14:08:44 +0200

    dz work

---
+++ lesson1/lesson2/yo67.py
@@ -0,0 +1,19 @@
+user_1 = { 
+    "Name" : "Tom" ,
+    "age" : 21 ,
+    "balance" : 10000 ,
+}
+# ///000000000000000000000000///
+
+user_2 = {
+    "Name" : "Joui" ,
+    "age" : 56 ,
+    "balance" : 47000 ,
+}
+
+print(user_1)
+
+
+
+
+
