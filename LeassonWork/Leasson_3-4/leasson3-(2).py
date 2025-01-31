75cc8e866cd6eb5006c0c5ba75234401a710367c
HackHunter <pedalimagazine@gmail.com>
2025-01-31 10:53:47 +0200

    232

---
+++ LeassonWork/Leasson_3-4/leasson3-(2).py
@@ -0,0 +1,53 @@
+user1 = {
+
+"Name:" : "Dima",
+"Country:" : "UA",
+"UserName @ " : "@duilo765",
+"Balance :": 10000,
+"Status" : True,
+}
+
+print(user1)
+# ======================================
+
+user2 = {
+
+"Name:" : "Slavik",
+"Country:" : "UA",
+"UserName @ " : "@tery6789",
+"Balance :": 1003243200,
+"Status" : True,
+}
+
+print(user2)
+# ===============================
+
+user3 = {
+
+"Name:" : "Anya",
+"Country:" : "UA",
+"UserName @ " : "@princhesa5677",
+"Balance :": 000,
+"Status" : False,
+}
+
+print(user3)
+# ==============================================
+
+user4 = {
+
+"Name:" : "Tolik",
+"Country:" : "UA",
+"UserName @ " : "@chip5",
+"Balance :": 12321312,
+"Status" : True,
+}
+
+print(user4)
+# ----------------------------------------------------------------------
+
+if user1 ["Country:"] == "UA" and user1["Balance :"] > 0:
+    print(user1["UserName @ "])
+    print("User registry")
+else:
+    print("End")
